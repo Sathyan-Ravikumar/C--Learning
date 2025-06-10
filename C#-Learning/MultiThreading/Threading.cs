68fae02030d302a4fcdbedb92099d6ab2f0bc2fb
Sathyan RaviKumar <sathyan.ravikumar@kanini.com>
2025-06-10 12:59:49 +0530

    Sleep,Delay

--- C#-Learning/MultiThreading/Threading.cs
+++ C#-Learning/MultiThreading/Threading.cs
@@ -19,23 +19,24 @@ namespace C__Learning.MultiThreading
 
             await AnotherThread();
             await Task.Run(() => Thread2());
+            //tpl- Task Parallel Library
+            //sleep, lock - file based(error in shared resourse)
         }
 
         static async Task AnotherThread()
         {
             Console.WriteLine($"Thread ID: {Thread.CurrentThread.ManagedThreadId}");
 
-            await Task.Delay(2000);
+             Thread.Sleep(2000); // blocks the current thread and resumes execution on the same thread after the time completes
 
             Console.WriteLine($"[Another Thread] Thread ID: {Thread.CurrentThread.ManagedThreadId}");
         }
 
-        
             static async void Thread2()
             {
                 Console.WriteLine($"New Thread ID: {Thread.CurrentThread.ManagedThreadId}");
-                await Task.Delay(1000);
-                Console.WriteLine($"After Await New Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+                await Task.Delay(1000); //is non-blocking; it delays asynchronously and may resume on the same or a different thread from the thread pool.
+            Console.WriteLine($"After Await New Thread ID: {Thread.CurrentThread.ManagedThreadId}");
             }
 
         
